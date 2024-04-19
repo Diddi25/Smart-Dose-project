46f82047f00085602ce65bbaa77de5d9b94b5a17
Diddi <diddi.dutta@outlook.com>
2024-04-19 15:32:25 +0200

    succeeded to fetch ip address of user

--- Smart-Dose-UI/src/views/AccountView.jsx
+++ Smart-Dose-UI/src/views/AccountView.jsx
@@ -1,7 +1,35 @@
 
+import { useState, useEffect } from "react";
 import "../css/account.css";
 
 function AccountView(props) {
+    const [ipAddress, setIpAdderess] = useState('');
+    const [geoInfo, setGeoInfo] = useState({});
+
+    useEffect(() => {
+        getVisitorIP();
+    }, []);
+
+    const getVisitorIP = async () => {
+        try {
+            const response = await fetch('https://api.ipify.org');
+            const data = await response.text();
+            setIpAdderess(data);
+        } catch (error) {
+            console.error('Failed to fetch IP:', error);
+        }
+    }
+
+    const fetchIPInfo = async () => {
+        try {
+            const response = await fetch('http://ip-api.com/json/130.229.182.157');
+            const data = await response.json();
+            setGeoInfo(data);
+        } catch (error) {
+            console.error('Failed to fetch location info:', error);
+        }
+    };
+
     return (
         <div>
             <div>
@@ -12,6 +40,20 @@ function AccountView(props) {
             <div>
                 <h5>Welcome Diddi</h5>
                 <p>{/*google account name*/}</p>
+            </div>
+            <div>
+                <button>Min ip adress: {ipAddress}</button>
+                <button>Fetch {fetchIPInfo}</button>
+            </div>
+            <div>
+                <button>
+                    Here:
+                    {geoInfo.country && (
+                        <div>
+                            <strong>Country: </strong> {geoInfo.country}
+                        </div>
+                    )}
+                </button>
             </div>
             <div>
                 <h6>
