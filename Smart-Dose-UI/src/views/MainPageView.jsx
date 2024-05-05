3f2c32be0d4646ac125cb6608b5a10dab5a1ac6c
HassBud <jennifer@bude.se>
2024-05-05 22:58:55 +0200

    Onepage homepage

--- Smart-Dose-UI/src/views/MainPageView.jsx
+++ Smart-Dose-UI/src/views/MainPageView.jsx
@@ -36,6 +36,65 @@ function MainPageView(props) {
 
     
     return (
+        <div className="main">
+        <div className="header">
+        <header >
+            SMART DOSE
+        </header>
+        <h6>“Precision in Every Wash”</h6>
+        </div>
+        <div className="card-container">
+      
+            <div className="card">
+                DETERGENT
+                <br/>
+                <br/>
+                <div className="main-button">
+                <button id="white" onClick={() => buttonClickHandlerDetergent("white")}disabled={activeButtonDetergent === "white"}>WHITE</button>
+                <button id="color" onClick ={() => buttonClickHandlerDetergent("color")} disabled ={activeButtonDetergent === "color"}>COLOR</button>
+                </div>
+               
+            </div>
+            <div className="card">
+            <div className="water-hardness">
+
+                WATER HARDNESS <br/>
+                <br/>
+                <br/>
+               <h6>Current location:</h6>
+                <select className="dropdown">
+                    <option>Stockholm 4-6° dH</option>
+                </select>
+            </div>
+            </div>
+            <div className="card">
+            SELECT WEIGHT
+            <div className="main-button">
+                <button id="0-3" onClick={() => buttonClickHandlerWeight("0-3")} disabled={activeButtonWeight === "0-3"}>0-3 kg</button>
+                <button id="4-5" onClick={() => buttonClickHandlerWeight("4-5")} disabled={activeButtonWeight === "4-5"}>4-5 kg</button>
+                <button id="6+" onClick={() => buttonClickHandlerWeight("6+")} disabled={activeButtonWeight === "6+"}>6+ kg</button>
+                <h6>OR use scaling device</h6>
+                <button id="scale" onClick={() => {buttonClickHandlerWeight("scale");handleScaleWeightACB();}} disabled={activeButtonWeight === "scale"}>SCALE</button>
+                <div>
+                <input className="dropdown" type="number" value={props.weight} onChange={handleScaleWeightACB} name="quantity" min="0" placeholder="Scale weight.."  readOnly/>
+                </div>
+            </div>
+            </div>
+            <div className="card">
+                START / STOP
+                <br/>
+                <br/>
+                <div className="ss-button">
+                <br />
+                <button id="start" onClick ={() => setButtonDisabled(true)} disabled ={isButtonDisabled} >START</button>
+                <button id="cancel" onClick ={() => setButtonDisabled(false)} disabled ={!isButtonDisabled}>CANCEL</button>
+    
+            </div>
+            </div>
+            </div>
+        </div>
+
+        /* 
         <div>
             <div className="header">
                 <h1>SMART DOSE</h1>
@@ -90,6 +149,7 @@ function MainPageView(props) {
                 Software licences @ [footer]
             </footer>
         </div>
+        */
     );
 
 }
