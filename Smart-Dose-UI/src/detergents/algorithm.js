55cce69b7d13074df5902d5575828d9ae100168e
Harry <tumbaharry@gmail.com>
2024-05-09 11:22:44 +0200

    main algoritm detergent

--- Smart-Dose-UI/src/detergents/algorithm.js
+++ Smart-Dose-UI/src/detergents/algorithm.js
@@ -1,55 +1,24 @@
-function calculateDetergent(detergent, weight, waterHardness) {
-    let lowerDosage, upperDosage;
 
-    if (weight <= 3) {
-        lowerDosage = 0;
-        upperDosage = parseFloat(detergent.dosageTable[waterHardness]["0-3kg"]);
-    } else if (weight <= 6) {
-        lowerDosage = parseFloat(detergent.dosageTable[waterHardness]["0-3kg"]);
-        upperDosage = parseFloat(detergent.dosageTable[waterHardness]["4-6kg"]);
-    } else if (weight <= 9) {
-        lowerDosage = parseFloat(detergent.dosageTable[waterHardness]["4-6kg"]);
-        upperDosage = parseFloat(detergent.dosageTable[waterHardness]["7-9kg"]);
-    } else {
-        lowerDosage = parseFloat(detergent.dosageTable[waterHardness]["7-9kg"]);
-        upperDosage = lowerDosage;
+function mainAlgoritm(detergent, weight, waterHardness) {
+    switch(detergrnt.brand.toLowerCase()) {
+        // Case algoritm för ica
+        case 'ica':
+            return ica.calculateDetergent(detergent, weight, waterHardness);
+        // Case algoritm för A+ 
+        case 'a+':
+            return Aplus.calculateDetergent(detergent, weight, waterHardness);
+        // Case algoritm för Ica Basic
+        case 'icabasic':
+            return IcaBasic.calculateDetergent(detergent, weight, waterHardness);
+        // Case algoritm för Ica Skona
+        case 'icaskona':
+            return IcaSkona.calculateDetergent(detergent, weight, waterHardness);
+        // Case algoritm för Via    
+        case 'Via':
+            return Via.calculateDetergent(detergent, weight, waterHardness);
+
+
+        default:
+            return "Ingen hantering för det angivna varumärket.";
     }
-
-    // Interpolate between the dosages based on the given weight
-    const lowerWeight = Math.floor(weight / 3) * 3;
-    const upperWeight = Math.ceil(weight / 3) * 3;
-    const exactDosage = lowerDosage + ((weight - lowerWeight) / (upperWeight - lowerWeight)) * (upperDosage - lowerDosage);
-
-    return `${exactDosage} ml`;
 }
-
-// Example usage:
-const detergent = {
-    id: "ID",
-    name: "Detergent Name",
-    brand: "Brand Name",
-    type: "color/White",
-    weight: "1.9kg",
-    articleNumber: "Article Number",
-    dosageTable: {
-        "Soft Water": {
-            "0-3kg": "20 ml",
-            "4-6kg": "50 ml",
-            "7-9kg": "75 ml"
-        },
-        "Medium Water": {
-            "0-3kg": "30 ml",
-            "4-6kg": "70 ml",
-            "7-9kg": "105 ml"
-        },
-        "Hard Water": {
-            "0-3kg": "40 ml",
-            "4-6kg": "80 ml",
-            "7-9kg": "120 ml"
-        }
-    }
-};
-
-const weight = 6; // kg
-const waterHardness = "Medium Water";
-console.log("Recommended dosage:", calculateDetergent(detergent, weight, waterHardness));
