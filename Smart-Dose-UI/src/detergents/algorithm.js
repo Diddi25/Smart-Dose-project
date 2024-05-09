7b2105ecede5ab79a45ae0fab02556927f9d6243
Harry <tumbaharry@gmail.com>
2024-05-09 11:41:05 +0200

    main algorithm new

--- Smart-Dose-UI/src/detergents/algorithm.js
+++ Smart-Dose-UI/src/detergents/algorithm.js
@@ -1,9 +1,11 @@
 
-function mainAlgoritm(detergent, weight, waterHardness) {
+function mainAlgoritm(detergent, weight, hardness) {
+    const waterHardness = hardnessToString(hardness);
+
     switch(detergrnt.brand.toLowerCase()) {
         // Case algoritm för ica
         case 'ica':
-            return ica.calculateDetergent(detergent, weight, waterHardness);
+            return ica.calculateDetergent(detergent, weight, waterHardness );
         // Case algoritm för A+ 
         case 'a+':
             return Aplus.calculateDetergent(detergent, weight, waterHardness);
@@ -18,8 +20,18 @@ function mainAlgoritm(detergent, weight, waterHardness) {
             return Via.calculateDetergent(detergent, weight, waterHardness);
         //test
 
-
         default:
             return "Ingen hantering för det angivna varumärket.";
     }
+}
+
+function hardnessToString(hardness){
+    if (hardness.Hardness < 8){
+        return "Soft Water <8°dH"; 
+    } else if (hardness.Hardness <=14){
+        return "Medium Water 8-14°dH"
+    } else {
+        return "Hard Water >14°dH"
+    }
+
 }
