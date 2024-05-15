8fb139b9f54e381c3e99752c72a4c7c9972bcdb9
Samuel Lövkvist <lovkv@kth.se>
2024-05-15 17:09:00 +0200

    instruction-text

--- Smart-Dose-UI/src/views/InstructionPageView.jsx
+++ Smart-Dose-UI/src/views/InstructionPageView.jsx
@@ -16,7 +16,10 @@ function InstructionPageView(props) {
                         <div className="instr-text">
                             <h2> STEP 1</h2>
                             <h3>SELECT DETERGENT TYPE</h3>
-                            <p>Start by placing the detergent cup under your preferred dispenser (white or color). If it's your first time, enter the detergent you've filled the dispenser with. Otherwise, your pre-selected detergent will be automatically chosen.</p>
+                            <p>Start by placing the detergent cup under your preferred dispenser (white or color). On the HOME page select the prefered detergent.
+                            <br/><br/><br/>
+                                If it's your first time, enter the detergent you've filled the dispenser with. Otherwise, your pre-selected detergent will be automatically chosen.
+                            </p>
                         </div>
                     </div>
                 </div>
